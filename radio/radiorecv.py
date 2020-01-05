e744c4ca0df2b26cdc8978f709d9bb7ba7c2b7c1
Sick <chezsick@bbox.fr>
2020-01-05 17:21:48 +0100

    fix radiorecv

--- radio/radiorecv.py
+++ radio/radiorecv.py
@@ -2,5 +2,5 @@ import sys
 min = int(sys.argv[1])
 max = int(sys.argv[2])
 for o in sys.stdin:
- if int(o) >= min and int(o) <= max:
+ if int(o) >= min and int(o) < max:
   sys.stdout.write(chr(int((256.0*(int(o)-min)/(float(max)-float(min))))))
