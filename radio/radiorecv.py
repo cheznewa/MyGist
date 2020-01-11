37dae7ab50db38a434de7b114129f8eccc7c4c39
Sick <chezsick@bbox.fr>
2020-01-11 15:35:32 +0100

    add 16 bit support for radio

--- radio/radiorecv.py
+++ radio/radiorecv.py
@@ -3,4 +3,10 @@ min = int(sys.argv[1])
 max = int(sys.argv[2])
 for o in sys.stdin:
  if int(o) >= min and int(o) < max:
-  sys.stdout.write(chr(int((256.0*(int(o)-min)/(float(max)-float(min))))))
+  if sys.argv[3] == "16":
+   a = int((65536.0*(int(o)-min)/(float(max)-float(min))))
+   p = int(a % 256)
+   q = int(a / 256)
+   sys.stdout.write("%s%s" %(chr(p),chr(q)))
+  if sys.argv[3] == "8":
+   int((256.0*(int(o)-min)/(float(max)-float(min))))
--- radio/radiosend.py
+++ radio/radiosend.py
@@ -1,6 +1,11 @@
 import sys
 min = int(sys.argv[1])
 max = int(sys.argv[2])
-while True:
- o = sys.stdin.read(1)
- sys.stdout.write("%s\n" %(int((ord(o)/256.0)*(max-min))+min))
+if sys.argv[3] == "16":
+ while True:
+  o = sys.stdin.read(2)
+  sys.stdout.write("%s\n" %(int(((ord(o[0])/65536.0)+(ord(o[1])/256.0))*(max-min))+min))
+if sys.argv[3] == "8":
+ while True:
+  o = sys.stdin.read(1)
+  sys.stdout.write("%s\n" %(int((ord(o)/256.0)*(max-min))+min))
