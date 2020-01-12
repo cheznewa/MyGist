538c4690c018a172c98532c0f675b7338dcbe248
Sick <chezsick@bbox.fr>
2020-01-12 02:18:49 +0100

    update radio from decimal to base58

--- radio/radiorecv.py
+++ radio/radiorecv.py
@@ -1,12 +1,13 @@
 import sys
+from base58 import b58decode_int
 min = int(sys.argv[1])
 max = int(sys.argv[2])
 for o in sys.stdin:
  if int(o) >= min and int(o) < max:
   if sys.argv[3] == "16":
-   a = int((65536.0*(int(o)-min)/(float(max)-float(min))))
+   a = int((65536.0*(int(b58decode_int(o))-min)/(float(max)-float(min))))
    p = int(a % 256)
    q = int(a / 256)
    sys.stdout.write("%s%s" %(chr(p),chr(q)))
   if sys.argv[3] == "8":
-   int((256.0*(int(o)-min)/(float(max)-float(min))))
+   int((256.0*(int(b58decode_int(o))-min)/(float(max)-float(min))))
--- radio/radiosend.py
+++ radio/radiosend.py
@@ -1,11 +1,12 @@
 import sys
+from base58 import b58encode_int
 min = int(sys.argv[1])
 max = int(sys.argv[2])
 if sys.argv[3] == "16":
  while True:
   o = sys.stdin.read(2)
-  sys.stdout.write("%s\n" %(int(((ord(o[0])/65536.0)+(ord(o[1])/256.0))*(max-min))+min))
+  sys.stdout.write("%s\n" %(b58encode_int(int(((ord(o[0])/65536.0)+(ord(o[1])/256.0))*(max-min))+min)))
 if sys.argv[3] == "8":
  while True:
   o = sys.stdin.read(1)
-  sys.stdout.write("%s\n" %(int((ord(o)/256.0)*(max-min))+min))
+  sys.stdout.write("%s\n" %(b58encode_int(int((ord(o)/256.0)*(max-min))+min)))
