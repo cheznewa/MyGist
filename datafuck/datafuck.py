90796cdcdb7bfa316917921998ea0d9393018c76
Sick <chezsick@bbox.fr>
2020-03-03 14:43:09 +0100

    fix it

--- datafuck/datafuck.py
+++ datafuck/datafuck.py
@@ -13,4 +13,4 @@ for p in range(int(sys.argv[2]),int(sys.argv[3])):
  o = 0.0
  for q in range(int(rex[1])):
   o = o + float(a[q][p/(pow(int(rex[0]),q))%int(rex[0])])
- sys.stdout.write(chr(int(o)))
+ sys.stdout.write(chr(int(o)%256))
