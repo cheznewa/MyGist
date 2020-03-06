693c4090a3fe14f63b2ff54aa578cfa3bbed4c1e
Sick <chezsick@bbox.fr>
2020-03-06 02:11:25 +0100

    add BLE TOU Time And Date

---
+++ btdatetime/getdatetimebt.php
@@ -0,0 +1,18 @@
+<?php
+if (!isset($argv[1]))
+{
+$argv[1] = time();
+}
+function getdatebt($timestamps)
+{
+$jour = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samdi","Dimance"];
+$sem = $jour;
+return intval($timestamps / 49) + 1 . " Foitierre " .$sem[intval(($timestamps % 49) / 7)] .",". $jour[$timestamps % 7];
+}
+function gettimeday($time)
+{
+$r = intval($time / 2);
+$r .= boolval($time % 2) ? ":" : "";
+return $r;
+}
+echo getdatebt(intval(($argv[1] - 986792400) / 86400))." ".gettimeday(($argv[1] - 18000) % 86400)."\n";
