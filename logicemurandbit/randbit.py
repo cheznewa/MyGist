53888c7a40e5846ccb02d7ed9fee57c117e911dc
Sick <chezsick@bbox.fr>
2020-03-07 02:26:49 +0100

    add fd 3 in randbit

--- logicemurandbit/randbit.py
+++ logicemurandbit/randbit.py
@@ -1,9 +1,10 @@
 import sys
 import re
-fs = open(sys.argv[1],"rb")
+import os
+# randbit.py < circuit 3< randomsource
 for o in sys.stdin.read():
  if o == "?":
-  if bool(ord(fs.read(1))%2):
+  if bool(ord(os.read(3,1))%2):
    sys.stdout.write("F")
   else:
    sys.stdout.write("f")
