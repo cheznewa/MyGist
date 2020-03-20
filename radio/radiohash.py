89a380411174f2e28a472ef78f593706997ed63b
Sick <chezsick@bbox.fr>
2020-03-20 04:50:59 +0100

    update radiohash

--- radio/radiohash.py
+++ radio/radiohash.py
@@ -2,13 +2,13 @@ import sys
 from base58 import b58decode_int
 min = int(sys.argv[1])
 max = int(sys.argv[2])
-j = max - min
-hash = [0] * j
+len = int(sys.argv[3])
+hash = [0] * len
 for o in sys.stdin:
  if int(b58decode_int(o)) >= min and int(b58decode_int(o)) < max:
-  i = b58decode_int(o)-min
+  i = int((len*(int(b58decode_int(o))-min)/(float(max)-float(min))))
   hash[i] = (hash[i] + 1) % 10
 
-for u in range(j):
+for u in range(len):
  sys.stdout.write("%s" %(hash[u]))
 sys.stdout.write("\n")
