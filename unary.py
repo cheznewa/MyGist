d686247d39ab6980dd76ff3acb887fd23c82d376
Sick <chezsick@bbox.fr>
2020-04-03 23:10:38 +0200

    change unary

--- unary.py
+++ unary.py
@@ -1,9 +1,13 @@
 import sys
 n = 0
+p = 0
 for h in sys.stdin.read():
  if h == "1":
   n = n + 1
+  if p > 0:
+   n = n + pow(int(sys.argv[1]),p)
+   p = 0
  if h == ".":
-  n = n + int(sys.argv[1])
+  p = p + 1
 
 sys.stdout.write(str(n))
