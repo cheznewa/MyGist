745cecf4277858861ec251fdfe31c087e77ef274
Sick <chezsick@bbox.fr>
2020-04-08 05:06:25 +0200

    add luhn58

---
+++ luhn58.py
@@ -0,0 +1,16 @@
+import base58
+import sys
+n = 0
+s = 0
+for g in sys.stdin.read():
+ if bool(n % 2):
+  l = 2 * base58.b58decode_int(g)
+  if l > 57:
+   l = l - 57
+  s = s + l
+ else:
+  s = s + base58.b58decode_int(g)
+ n = n + 1
+ sys.stdout.write(g)
+
+sys.stdout.write(chr(base58.b58encode_int(s % 58)[0]))
