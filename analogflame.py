d9c55df0f4c4594885a369858798cccb5f059450
Newa <chezsick@orange.fr>
2020-05-03 02:14:04 +0200

    fix it

--- analogflame.py
+++ analogflame.py
@@ -1,3 +1,4 @@
+# Télévision Render From TV's Phelÿn
 import sys
 r = 0
 g = 0
@@ -8,11 +9,23 @@ r = 256
 while True:
  o = sys.stdin.read(3)
  t += abs(r - ord(o[0])) + abs(g - ord(o[1])) + abs(b - ord(o[2]))
- rr = (int((flame[0]*(t/1024.0) + ord(o[0])))) % 256
+ rr = (int((flame[0]*(t/1024.0) + ord(o[0]))))
+ if rr >= 255:
+  rr = 255
+ if rr <= 0:
+  rr = 0
  sys.stdout.write(chr(rr))
- rg = (int((flame[1]*(t/1024.0) + ord(o[1])))) % 256
+ rg = (int((flame[1]*(t/1024.0) + ord(o[1]))))
+ if rg >= 255:
+  rg = 255
+ if rg <= 0:
+  rg = 0
  sys.stdout.write(chr(rg))
- rb = (int((flame[2]*(t/1024.0) + ord(o[2])))) % 256
+ rb = (int((flame[2]*(t/1024.0) + ord(o[2]))))
+ if rb >= 250:
+  rb = 255
+ if rb <= 0:
+  rb = 0
  sys.stdout.write(chr(rb))
  if t >= 0:
   t = t - r
