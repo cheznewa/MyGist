8d97535bf1bd0b059b3ab815082ab26ccf7cc162
Newa <chezsick@orange.fr>
2020-05-05 19:15:17 +0200

    update knutharrow

--- knutharrow/knutharrow.py
+++ knutharrow/knutharrow.py
@@ -6,8 +6,8 @@ m = sys.argv[4]
 if m[-1:] == "b":
  m = pow(2,int(m[:-1]))-1
 x = a
-for l in range(z):
- for n in range(b):
+for l in range(z-1):
+ for n in range(b-1):
   x = pow(a,x,int(m))
  a = x
 
