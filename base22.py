b831ad49b3e5921fc23d87b26e38abdbb66186ef
Newa <chezsick@orange.fr>
2020-06-21 05:49:27 +0200

    update base22

--- base22.py
+++ base22.py
@@ -24,9 +24,40 @@ wordbase = [
 "Electrol",
 "Electral"
 ]
+letterbase = [
+"U",
+"E",
+"DE",
+"TE",
+"O",
+"DO",
+"TO",
+"A",
+"DA",
+"TA",
+"SE",
+"SDE",
+"STE",
+"SO",
+"SDO",
+"STO",
+"SA",
+"SDA",
+"STA",
+"EL",
+"OL",
+"AL"
+]
 out = ""
-while a:
- a,int = divmod(a,22)
- out = wordbase[int] + " " + out
+
+if sys.argv[1] == "letter":
+ while a:
+  a,int = divmod(a,22)
+  out = letterbase[int] + out
+
+if sys.argv[1] == "word":
+ while a:
+  a,int = divmod(a,22)
+  out = wordbase[int] + " " + out
 
 sys.stdout.write(out)
