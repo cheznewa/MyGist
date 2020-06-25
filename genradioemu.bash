76a096cb61481cbb0cbf372d8e64b6c960812c67
Newa <chezsick@orange.fr>
2020-06-25 20:50:35 +0200

    update genradioemu

--- genradioemu.bash
+++ genradioemu.bash
@@ -7,8 +7,9 @@ sox -t mp3 http://generationfm.ice.infomaniak.ch/generationfm-high.mp3 -t u16 -r
 sox -t mp3 http://live02.rfi.fr/rfimonde-96k.mp3 -t u16 -r 12000 -c 1 - trim 0 $timing | python $MYGIST/radio/radiosend.py 8900000 8910000 16 > $tmp/rfi.radio
 sox -t mp3 http://broadcast.infomaniak.net/tsfjazz-high.mp3 -t u16 -r 12000 -c 1 - trim 0 $timing | python $MYGIST/radio/radiosend.py 8990000 9000000 16 > $tmp/tsfjazz.radio
 sox -t mp3 https://scdn.nrjaudio.fm/audio1/fr/30601/mp3_128.mp3 -t u16 -r 12000 -c 1 - trim 0 $timing | python $MYGIST/radio/radiosend.py 9040000 9050000 16 > $tmp/nostalgie.radio
-sox -t mp3 http://stream1.evasionfm.com/Chante_France -t u16 -r 12000 -c 1 - trim 0 $timing | python $MYGIST/radio/radiosend.py 9040000 9050000 16 > $tmp/chantefrance.radio
+sox -t mp3 http://stream1.evasionfm.com/Chante_France -t u16 -r 12000 -c 1 - trim 0 $timing | python $MYGIST/radio/radiosend.py 9090000 9100000 16 > $tmp/chantefrance.radio
 sox -t mp3 http://icecast.skyrock.net/s/natio_mp3_128k -t u16 -r 12000 -c 1 - trim 0 $timing | python $MYGIST/radio/radiosend.py 9600000 9610000 16 > $tmp/skyrock.radio
+sox -t mp3 http://broadcast.infomaniak.net/start-voltage-high.mp3 -t u16 -r 12000 -c 1 - trim 0 $timing | python $MYGIST/radio/radiosend.py 9690000 9700000 16 > $tmp/voltage.radio
 sox -t mp3 https://scdn.nrjaudio.fm/audio1/fr/30401/mp3_128.mp3 -t u16 -r 12000 -c 1 - trim 0 $timing | python $MYGIST/radio/radiosend.py 9740000 9750000 16 > $tmp/rireetchansons.radio
 sox -t mp3 https://start-sud.ice.infomaniak.ch/start-sud-high.mp3 -t u16 -r 12000 -c 1 - trim 0 $timing | python $MYGIST/radio/radiosend.py 9990000 10000000 16 > $tmp/sudradio.radio
 sox -t mp3 http://streaming.radio.rtl.fr/rtl-1-44-128 -t u16 -r 12000 -c 1 - trim 0 $timing | python $MYGIST/radio/radiosend.py 10430000 10440000 16 > $tmp/rtl.radio
