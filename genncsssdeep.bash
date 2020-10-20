a5d40d4f5b41427e7735d95a9007484690a2b0aa
Newa <chezsick@orange.fr>
2020-10-20 20:46:29 +0200

    add NCS SSDeep Generator

---
+++ genncsssdeep.bash
@@ -0,0 +1,12 @@
+# Pour Générer Un Fichier SSDEEP Contient Touts Les NoCopyrightsounds En Pitch Afin De Identifier Une Musique NCS
+# Premier Argument Est La Sortie
+# Usage Après Génération ::::::::::::::: aubio pitch mamusique.mp3 | cut -d "	" -f 2 | ssdeep -m votressdeepgenerer.txt
+# Requis ::::: Aubio et SSDeep
+tmp=$(mktemp -d)
+youtube-dl -o "$tmp/%(title)s.%(ext)s" -i https://soundcloud.com/nocopyrightsounds
+for u in $tmp/*
+do
+aubio pitch "$u" | cut -d "	" -f 2 > "$u.pitch"
+done
+ssdeep -b $tmp/*.pitch > "$1"
+rm -r $tmp
--- iptvci/web2tvtxt.bash
+++ iptvci/web2tvtxt.bash
@@ -1,13 +1,13 @@
-text=$(mktemp)
 render=$(mktemp)
-curl -s "$1" | vilistextum - $text
+title=$(mktemp)
 if test $2 = A
 then
-gm convert -font /usr/local/share/fonts/EuropeanTeletext.ttf -pointsize 16 -size 768x5760 -background black -fill white "caption:@$text" miff:$render
+font=/usr/local/share/fonts/EuropeanTeletext.ttf
 fi
 if test $2 = B
 then
-gm convert -font /usr/local/share/fonts/EuropeanTeletextNuevo.ttf -pointsize 16 -size 768x5760 -background black -fill white "caption:@$text" miff:$render
+font=/usr/local/share/fonts/EuropeanTeletextNuevo.ttf
 fi
-rm $text
-convert $render -crop 768x576 "gif:$3"
+gm convert -font $font -pointsize 16 -size 768x560 -background black -fill white "caption:$(curl -s '$1' | vilistextum - -" miff:$render
+gm convert -font $font -pointsize 16 -size 768x16  -background white -fill black "caption:'$3' - $(date)" miff:$title
+gm convert -append $reader $title "$4"
