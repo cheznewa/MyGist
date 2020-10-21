5e65e143b7ee7280b6397a567f39dfbf1738b190
Newa <chezsick@orange.fr>
2020-10-22 00:42:10 +0200

    Update NCS SSDeep Generator

--- genncsssdeep.bash
+++ genncsssdeep.bash
@@ -1,6 +1,6 @@
 # Pour Générer Un Fichier SSDEEP Contient Touts Les NoCopyrightsounds En Pitch Afin De Identifier Une Musique NCS
 # Premier Argument Est La Sortie
-# Usage Après Génération ::::::::::::::: aubio pitch mamusique.mp3 | cut -d "	" -f 2 | cur -d "." -f 1 | ssdeep -m votressdeepgenerer.txt
+# Usage Après Génération ::::::::::::::: aubio pitch mamusique.mp3 | cut -d "	" -f 2 | cut -d "." -f 1 | ssdeep -m votressdeepgenerer.txt
 # Requis ::::: Aubio et SSDeep
 tmp=$(mktemp -d)
 youtube-dl -o "$tmp/%(title)s.%(ext)s" -i https://soundcloud.com/nocopyrightsounds
