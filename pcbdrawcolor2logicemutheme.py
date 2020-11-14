1a28a3f5aed01252cc5d18c4f7ef63d544ae1a90
Newa <chezsick@orange.fr>
2020-11-14 19:24:57 +0100

    update pcbdraw2themelogicemu

--- pcbdrawcolor2logicemutheme.py
+++ pcbdrawcolor2logicemutheme.py
@@ -5,18 +5,34 @@ board = col["board"]
 copper = col["copper"]
 r = copper[1:3]
 r = int(r,16)*2
+if r > 255:
+ r = 255
 r = str(hex(r))[2:]
 g = copper[3:5]
 g = int(g,16)*2
+if g > 255:
+ g = 255
 g = str(hex(g))[2:]
 b = copper[5:7]
 b = int(b,16)*2
+if b > 255:
+ b = 255
 b = str(hex(b))[2:]
 copperactive = "#" + r + g + b
 pads = col["pads"]
 clad = col["clad"]
 vcut = col["vcut"]
 silk = col["silk"]
+r = pads[1:3]
+r = int(r,16)/2
+r = str(hex(r))[2:]
+g = pads[3:5]
+g = int(g,16)/2
+g = str(hex(g))[2:]
+b = pads[5:7]
+b = int(b,16)/2
+b = str(hex(b))[2:]
+labelbg = "#" + r + g + b
 sys.stdout.write("background:%s\n" %(board))
 sys.stdout.write("on:%s\n" %(copperactive))
 sys.stdout.write("off:%s\n" %(copper))
@@ -24,7 +40,7 @@ sys.stdout.write("gate_on:%s\n" %(clad))
 sys.stdout.write("gate_off:%s\n" %(clad))
 sys.stdout.write("gate_bg:%s\n" %(pads))
 sys.stdout.write("text_fg:%s\n" %(silk))
-sys.stdout.write("text_bg:%s\n" %(pads))
+sys.stdout.write("text_bg:%s\n" %(labelbg))
 sys.stdout.write("bus:%s,%s,%s,%s,%s,%s,%s,%s\n" %(copper,copper,copper,copper,copper,copper,copper,copper))
 sys.stdout.write("led_off_fg:#f00,#f80,#dd0,#0d0,#88f,#a0d,#f99,#eee,white,#000\n")
 sys.stdout.write("led_off_bg:%s,%s,%s,%s,%s,%s,%s,%s,#f00,%s\n" %(board,board,board,board,board,board,board,board,board))
