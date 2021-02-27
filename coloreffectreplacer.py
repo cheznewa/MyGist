e3e45a7c8d5e2e504874dbc3756819c596ed68b8
Newa <61405797+cheznewa@users.noreply.github.com>
2021-02-27 03:53:00 +0100

    Add Sepia Effect

--- coloreffectreplacer.py
+++ coloreffectreplacer.py
@@ -47,6 +47,21 @@ def bnwcol(col,r,g,b):
  vb = str(format(vb,"02x"))
  return vr + vg + vb
 
+def sepcol(col,r,g,b):
+ vr = int(col[0:2],16)
+ vg = int(col[2:4],16)
+ vb = int(col[4:6],16)
+ baser = (vr+vg+vb)/3
+ baseg = (vr+vg+vb)/4
+ baseb = (vr+vg)/6
+ vr = int(vr*float(1-(r/255.0)))+int(baser*float(r/255.0))
+ vg = int(vg*float(1-(g/255.0)))+int(baseg*float(g/255.0))
+ vb = int(vb*float(1-(b/255.0)))+int(baseb*float(b/255.0))
+ vr = str(format(vr,"02x"))
+ vg = str(format(vg,"02x"))
+ vb = str(format(vb,"02x"))
+ return vr + vg + vb
+
 import sys
 import string
 while True:
@@ -62,6 +77,8 @@ while True:
     col = xorcol(col,int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4]))
    if sys.argv[1] == "bnw":
     col = bnwcol(col,int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4]))
+   if sys.argv[1] == "sep":
+    col = sepcol(col,int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4]))
   sys.stdout.write("#")
   sys.stdout.write(col)
  else:
