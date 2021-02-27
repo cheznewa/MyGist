31124635971fee31a5f7d660a4f5da2cb39b395f
Newa <61405797+cheznewa@users.noreply.github.com>
2021-02-27 22:01:01 +0100

    Add Replace Color In ColorEffectReplace

--- coloreffectreplacer.py
+++ coloreffectreplacer.py
@@ -62,6 +62,25 @@ def sepcol(col,r,g,b):
  vb = str(format(vb,"02x"))
  return vr + vg + vb
 
+def rmpcol(col,a,b,f):
+ vr = int(col[0:2],16)
+ vg = int(col[2:4],16)
+ vb = int(col[4:6],16)
+ ar = int(a[0:2],16)
+ ag = int(a[2:4],16)
+ ab = int(a[4:6],16)
+ br = int(b[0:2],16)
+ bg = int(b[2:4],16)
+ bb = int(b[4:6],16)
+ z = float(max(f-(abs(vr-ar)+abs(vg-ag)+abs(vb-ab)),0))
+ vr = int(vr*float(1-(z/f)))+int(br*float(z/f))
+ vg = int(vg*float(1-(z/f)))+int(bg*float(z/f))
+ vb = int(vb*float(1-(z/f)))+int(bb*float(z/f))
+ vr = str(format(vr,"02x"))
+ vg = str(format(vg,"02x"))
+ vb = str(format(vb,"02x"))
+ return vr + vg + vb
+
 import sys
 import string
 while True:
@@ -79,6 +98,8 @@ while True:
     col = bnwcol(col,int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4]))
    if sys.argv[1] == "sep":
     col = sepcol(col,int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4]))
+   if sys.argv[1] == "rmp":
+    col = rmpcol(col,sys.argv[2],sys.argv[3],int(sys.argv[4]))
   sys.stdout.write("#")
   sys.stdout.write(col)
  else:
