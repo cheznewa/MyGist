0c36dc117a5ea3d7470ffe20a38be61315e0622e
Newa <61405797+cheznewa@users.noreply.github.com>
2021-03-25 05:58:05 +0100

    Update logicemu2luxar.py

--- logicemu2luxar.py
+++ logicemu2luxar.py
@@ -9,10 +9,14 @@ def getmusicluxar(c):
  logic = logic + c.count("a")
  logic = logic + c.count("e")
  logic = logic + c.count("o")
+ logic = logic + c.count("h")
+ logic = logic + c.count("z")
  logicinv = 0
  logicinv = logicinv + c.count("A")
  logicinv = logicinv + c.count("E")
  logicinv = logicinv + c.count("O")
+ logicinv = logicinv + c.count("H")
+ logicinv = logicinv + c.count("Z")
  inputi = 0
  inputi = inputi + c.count("^")
  inputi = inputi + c.count("v")
@@ -28,15 +32,19 @@ def getmusicluxar(c):
  switch = switch + c.count("S")
  switch = switch + c.count("p")
  switch = switch + c.count("P")
+ memory = 0
+ memory = memory + c.count("b")
+ memory = memory + c.count("B")
  led = 0
  led = led + c.count("l")
  led = led + c.count("L")
+ kinetic = c.count("K")
  alu = c.count("U")
  term = c.count("T")
  matrix = c.count("D")
  quartz = c.count("t")
- pist = ((alu%11) + (logic - logicinv)) * (ic/(icd+1)) + (term%3) + ((switch*2)+(led*3)) + (matrix%7) + (quartz%5) + (inputi - inputinv)
- return abs(pist%16)
+ pist = (((alu%11) + (logic - logicinv)) * (ic/(icd+1))) + (term%3) + ((switch*2)+(led*3)) + (matrix%7) + (quartz%5) + (inputi - inputinv) + (memory % 17) - kinetic
+ return abs(pist)%16
 luxartracks = ["Birth","First Step","Curiosity","Process","Will Free","Scar","Loneliness","Explorer","Chase","Revolution","Blooming","Torment","Awareness","Self-Confidence","Ambition","Responsability"]
 get = getmusicluxar(circ)
 print("La Musique Luxar Pour Ce Circuit Est ::::: " + luxartracks[get])
