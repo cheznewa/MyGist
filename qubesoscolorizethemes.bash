0a2e17369c7716bafd82ef762de417397e8a39f4
Newa <61405797+cheznewa@users.noreply.github.com>
2021-04-13 20:43:10 +0200

    Update qubesoscolorizethemes.bash

--- qubesoscolorizethemes.bash
+++ qubesoscolorizethemes.bash
@@ -1,9 +1,9 @@
 if test -z $MYGIST
 then
-echo "Please Declare MYGIST Variable When MyGist Repo On GitHub ::::: cheznewa/mygist"
+printf "Please Declare MYGIST Variable When MyGist Repo On GitHub ::::: cheznewa/mygist\nAnd Dont Forget Use \"sudo\" For Admin With -E MYGIST=... To Declare The Variable.\n"
 exit 1
 fi
-git clone https://github.com/B00merang-Project/Adwaita /tmp/standard
+git clone https://github.com/B00merang-Project/B00merang-Flat /tmp/B00merang-Flat
 acol=(
 cc0000\;Red
 f57900\;Orange
@@ -17,11 +17,13 @@ edd400\;Yellow
 for n in $(seq 0 7)
 do
 name=$(cut -d ";" -f 2 <<< ${acol[$n]})
-cp -r /tmp/standard /tmp/standard-${name}
+cp -r /tmp/B00merang-Flat /tmp/B00merang-Flat-${name}
 hex=$(cut -d ";" -f 1 <<< ${acol[$n]})
-python2 $MYGIST/coloreffectreplacer.py rmp "4A90D9" "$hex" 300 < /tmp/standard/gtk-3.0/gtk.css > /tmp/standard-${name}/gtk-3.0/gtk.css
-python2 $MYGIST/coloreffectreplacer.py rmp "4A90D9" "$hex" 300 < /tmp/standard/gtk-3.0/gtk-dark.css > /tmp/standard-${name}/gtk-3.0/gtk-dark.css
-python2 $MYGIST/coloreffectreplacer.py rmp "4A90D9" "$hex" 300 < /tmp/standard/gtk-2.0/gtkrc > /tmp/standard-${name}/gtk-2.0/gtkrc
-cp -r /tmp/standard-${name} /usr/share/themes
+python2 $MYGIST/coloreffectreplacer.py rmp "2081CE" "$hex" 200 < /tmp/B00merang-Flat/gtk-3.0/gtk.css > /tmp/B00merang-Flat-${name}/gtk-3.0/gtk.css
+python2 $MYGIST/coloreffectreplacer.py rmp "2081CE" "$hex" 200 < /tmp/B00merang-Flat/gtk-3.0/gtk-dark.css > /tmp/B00merang-Flat-${name}/gtk-3.0/gtk-dark.css
+python2 $MYGIST/coloreffectreplacer.py rmp "2081CE" "$hex" 200 < /tmp/B00merang-Flat/gtk-3.0/gtk-light.css > /tmp/B00merang-Flat-${name}/gtk-3.0/gtk-light.css
+python2 $MYGIST/coloreffectreplacer.py rmp "2081CE" "$hex" 200 < /tmp/B00merang-Flat/gtk-2.0/gtkrc > /tmp/B00merang-Flat-${name}/gtk-2.0/gtkrc
+cp -r /tmp/B00merang-Flat-${name} /usr/share/themes
+printf "The Color ${name} Has Installed\n"
 done
-echo "Please Edit .xsettingsd In Line Gtk/KeyThemeName=standard-(yourcolor) And Net/ThemeName=standard-(yourcolor)"
+printf "Please Edit .xsettingsd In Line Gtk/KeyThemeName=standard-(yourcolor) And Net/ThemeName=standard-(yourcolor)\n"
