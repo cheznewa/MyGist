c35fc20689857da35b94a74d8903c25bec6b41a0
Newa <61405797+cheznewa@users.noreply.github.com>
2021-04-27 09:25:19 +0200

    Update qubesoscolorizethemes.bash

--- qubesoscolorizethemes.bash
+++ qubesoscolorizethemes.bash
@@ -24,6 +24,18 @@ python2 $MYGIST/coloreffectreplacer.py rmp "2081CE" "$hex" 200 < /tmp/B00merang-
 python2 $MYGIST/coloreffectreplacer.py rmp "2081CE" "$hex" 200 < /tmp/B00merang-Flat/gtk-3.0/gtk-light.css > /tmp/B00merang-Flat-${name}/gtk-3.0/gtk-light.css
 python2 $MYGIST/coloreffectreplacer.py rmp "2081CE" "$hex" 200 < /tmp/B00merang-Flat/gtk-2.0/gtkrc > /tmp/B00merang-Flat-${name}/gtk-2.0/gtkrc
 python2 $MYGIST/coloreffectreplacer.py rmp "2081CE" "$hex" 200 < /tmp/B00merang-Flat/gnome-shell/gnome-shell.css > /tmp/B00merang-Flat-${name}/gnome-shell/gnome-shell.css
+for m in /tmp/B00merang-Flat/gtk-2.0/assets/*.png
+do
+convert $m -fuzz 20% -fill "#$hex" -opaque "#2081CE" /tmp/B00merang-Flat-${name}/gtk-3.0/assets/$(basename $m)
+done
+for m in /tmp/B00merang-Flat/gtk-3.0/assets/*.png
+do
+convert $m -fuzz 20% -fill "#$hex" -opaque "#2081CE" /tmp/B00merang-Flat-${name}/gtk-3.0/assets/$(basename $m)
+done
+for m in /tmp/B00merang-Flat/gnome-shell/assets/*.png
+do
+convert $m -fuzz 20% -fill "#$hex" -opaque "#2081CE" /tmp/B00merang-Flat-${name}/gnome-shell/assets/$(basename $m)
+done
 cp -r /tmp/B00merang-Flat-${name} /usr/share/themes
 printf "The Color ${name} Has Installed\n"
 done
