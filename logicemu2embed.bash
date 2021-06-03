efffab243988594b90bd836997dafad371b50a03
Newa <61405797+cheznewa@users.noreply.github.com>
2021-06-03 22:37:58 +0200

    Add LZComp In LogicEmu2Embed

--- logicemu2embed.bash
+++ logicemu2embed.bash
@@ -1,6 +1,6 @@
-# That Embed LogicEmu Circuit, Into A PNG File, And For Extract Read The Size In "Car" Using tail Command.
+# That Embed LogicEmu Circuit, Into A PNG File, And For Extract Read The Size At Quotient In "Car" Using tail Command.
 # Embed :::: logicemu2embed.bash nameofcircuit < thecircuit.txt > output.png
-# extract :: tail -c sizereadascar thepngfileasembed.png
+# extract :: tail -c sizereadquotienttaiascar thepngfileasembed.png | lzcomp -d
 img=$(mktemp)
 circ=$(mktemp)
 t=$(mktemp)
@@ -13,10 +13,11 @@ name=CircuitSansNom
 fi
 convert -size 200x50 xc:none -stroke black -fill white -draw "rectangle 0,0 199,49 circle 25,25 15,25" miff:$img
 convert -font OCRA -pointsize 9 -background none "label:$name" miff:- | composite -gravity center - $img miff:$t
-base64 -w0 > $circ
-s=$(base64 -d $circ | wc -c)
-n=$(base64 -d $circ | python2 $MYGIST/logicemu2luxar.py)
-convert -font OCRA -pointsize 9 -background none "label:${s} Car" miff:- | composite -gravity southeast - $t miff:$w
+cat | lzcomp -f -o $circ
+s=$(wc -c < $circ)
+r=$(lzcomp -d < $circ | wc -c)
+n=$(lzcomp -d < $circ | python2 $MYGIST/logicemu2luxar.py)
+convert -font OCRA -pointsize 9 -background none "label:${s}/${r} Car" miff:- | composite -gravity southeast - $t miff:$w
 convert -font OCRA -pointsize 9 -background none "label:${n}" miff:- | composite -gravity northeast - $w png:$x
-base64 -d $circ | cat $x -
+cat $x $circ
 rm $img $circ $t $x $w
