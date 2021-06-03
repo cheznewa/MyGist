2a6478e99e159d5caa67fa64db8cadeef8b36738
Newa <61405797+cheznewa@users.noreply.github.com>
2021-06-03 13:07:36 +0200

    Update logicemu2embed.bash

--- logicemu2embed.bash
+++ logicemu2embed.bash
@@ -1,12 +1,12 @@
 # That Embed LogicEmu Circuit, Into A PNG File, And For Extract Read The Size In "Car" Using tail Command.
-# Embed :::: logicemu2embed.bash output.png nameofcircuit < thecircuit.txt
+# Embed :::: logicemu2embed.bash nameofcircuit < thecircuit.txt > output.png
 # extract :: tail -c sizereadascar thepngfileasembed.png
 img=$(mktemp)
 circ=$(mktemp)
 t=$(mktemp)
+w=$(mktemp)
 x=$(mktemp)
-name=$2
-out=$1
+name=$1
 if test -z $name
 then
 name=CircuitSansNom
@@ -15,6 +15,8 @@ convert -size 200x50 xc:none -stroke black -fill white -draw "rectangle 0,0 199,
 convert -font OCRA -pointsize 9 -background none "label:$name" miff:- | composite -gravity center - $img miff:$t
 base64 -w0 > $circ
 s=$(base64 -d $circ | wc -c)
-convert -font OCRA -pointsize 9 -background none "label:${s} Car" miff:- | composite -gravity southeast - $t png:$x
-base64 -d $circ | cat $x - > $out
-rm $img $circ $t
+n=$(base64 -d $circ | python2 $MYGIST/logicemu2luxar.py)
+convert -font OCRA -pointsize 9 -background none "label:${s} Car" miff:- | composite -gravity southeast - $t miff:$w
+convert -font OCRA -pointsize 9 -background none "label:${n}" miff:- | composite -gravity northeast - $w png:$x
+base64 -d $circ | cat $x -
+rm $img $circ $t $x $w
