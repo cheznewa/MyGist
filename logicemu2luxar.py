f69939e57fe56149660bc46b38ec93c1c16ffaa9
Newa <chezsick@orange.fr>
2021-06-07 07:31:07 +0200

    Just A Sub ...

--- logicemu2luxar.py
+++ logicemu2luxar.py
@@ -3,10 +3,10 @@ import re
 circ = sys.stdin.read()
 def getmusicluxar(c):
  c = re.sub("\"[^\"]*\"","",c)
- indown = c.count("w") + c.count("v")
- inup = c.count("m") + c.count("^")
- inleft = c.count("<") + c.count("[")
- inright = c.count(">") + c.count("]")
+ indown = c.count("v") - c.count("w")
+ inup = c.count("^") - c.count("m")
+ inleft = c.count("<") - c.count("[")
+ inright = c.count(">") - c.count("]")
  return (((inleft/3)%2)*8)+(((inup/3)%2)*4)+(((inright/3)%2)*2)+((indown/3)%2)
 luxartracks = ["Birth","First Step","Curiosity","Process","Free Will","Scar","Loneliness","Explorer","Chase","Revolution","Blooming","Torment","Awareness","Self-Confidence","Ambition","Responsability"]
 get = getmusicluxar(circ)
