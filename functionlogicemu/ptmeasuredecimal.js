5ff0278c510b07d405589833cdc9d5da35215515
Newa <chezsick@orange.fr>
2021-08-06 01:34:09 +0200

    add first function for logicemu

---
+++ functionlogicemu/ptmeasuredecimal.js
@@ -0,0 +1,35 @@
+// Le PT, Une Unité De Mesure BLE TOU; 1 Mettre = 4.61 PT
+registerAluOverride(0,
+function(position,outdigit)
+{
+var pos = 0;
+for (var n=0;n<position.length;n++)
+{
+pos += position[n] << n;
+}
+if (pos == 0)
+{
+var fn = 4
+}
+else
+{
+pos = pos+1;
+var u = pos*pos;
+var uu = u.toString();
+var ll = uu.length;
+var ss = 0;
+for (var n=0;n<ll;n++)
+{
+ss += parseInt(uu[n]);
+}
+var fn = 10 - (ss % 10);
+if (fn == 10)
+{
+fn = 0;
+}
+}
+for (var n=0;n<4;n++)
+{
+outdigit[n] = (fn >> n)%2;
+}
+});
