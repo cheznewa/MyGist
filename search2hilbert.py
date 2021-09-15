61b38fcb6e03d22a53918ae36f8011e81e13acc9
Newa <chezsick@orange.fr>
2021-09-15 04:36:53 +0200

    Add Search2Hilbert

---
+++ search2hilbert.py
@@ -0,0 +1,10 @@
+import sys
+from hilbertcurve.hilbertcurve import HilbertCurve # https://github.com/galtay/hilbertcurve/releases/tag/v1.0.2
+hilbert = HilbertCurve(int(sys.argv[1]),2)
+o = sys.stdin.read(pow(pow(2,int(sys.argv[1])),2))
+p = 0
+while True:
+ p = o.find(sys.argv[2],p)
+ c = hilbert.coordinates_from_distance(p)
+ sys.stdout.write("<span style=\"position:absolute;top:%s;left:%s;\">x</span>\n" %(c[0],c[1]))
+ p = p + len(sys.argv[2])
