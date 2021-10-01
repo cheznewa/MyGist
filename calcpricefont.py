296932d2add5c3cd9531d139ab826232db715c41
Newa <chezsick@orange.fr>
2021-10-01 10:03:53 +0200

    add Price Font Calculator

---
+++ calcpricefont.py
@@ -0,0 +1,12 @@
+import sys
+import re
+price=0
+for o in sys.stdin:
+ if re.search("<TTGlyph ",o):
+  price=price/1.1
+ if re.search("<pt ",o):
+  if re.search("<on=\"0\"",o):
+   price=price+0.2
+  else:
+   price=price+0.1
+sys.stdout.write("%s\n" %(price))
