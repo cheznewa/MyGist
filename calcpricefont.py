c076792a8ab396dfbeea83109a2faac07b7f0ac9
Newa <chezsick@orange.fr>
2021-10-01 10:07:20 +0200

    raak.

--- calcpricefont.py
+++ calcpricefont.py
@@ -5,7 +5,7 @@ for o in sys.stdin:
  if re.search("<TTGlyph ",o):
   price=price/1.1
  if re.search("<pt ",o):
-  if re.search("<on=\"0\"",o):
+  if re.search(" on=\"0\"",o):
    price=price+0.2
   else:
    price=price+0.1
