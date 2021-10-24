00689f64f09284d7ff4fa2f4f898d0a125c5f5b4
Newa <chezsick@orange.fr>
2021-10-24 02:26:28 +0200

    Update Check Beast Number

--- beastnumbercheck.py
+++ beastnumbercheck.py
@@ -6,21 +6,28 @@ for n in range(999):
  for i in o:
   s = (s+ord(i)+n) %999
  if s == 666:
-  sys.stdout.write("Beast Number Found For %s Sum\n" %(n))
+  sys.stdout.write("Beast Number Found At %s Sum\n" %(n))
+  f = f+1
+ s = 0
+for n in range(999):
+ for i in o:
+  s = (1+(s+ord(i)*n)) %999
+ if s == 666:
+  sys.stdout.write("Beast Number Found At %s Times\n" %(n))
   f = f+1
  s = 0
 for n in range(999):
  for i in o:
   s = (s+pow(ord(i),n,999))%999
  if s == 666:
-  sys.stdout.write("Beast Number Found For %s Power\n" %(n))
+  sys.stdout.write("Beast Number Found At %s Power\n" %(n))
   f = f+1
  s = 0
 for n in range(999):
  for i in o:
   s = (s+pow(n,ord(i),999))%999
  if s == 666:
-  sys.stdout.write("Beast Number Found For %s Expo\n" %(n))
+  sys.stdout.write("Beast Number Found At %s Expo\n" %(n))
   f = f+1
  s = 0
 
