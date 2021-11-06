5addc9d322fec69da9a4e7a28762c5e9651ce0a2
Newa <chezsick@orange.fr>
2021-11-06 16:58:33 +0100

    fix it

--- radio/radiorecvbinary.py
+++ radio/radiorecvbinary.py
@@ -7,16 +7,16 @@ c = ""
 b = [0] * 8
 while True:
  o = sys.stdin.read(4)
- n = unpack("I",o)[0]
- if n == min+1:
+ m = unpack("I",o)[0]
+ if m == min+1:
   for v in range(8):
    c = c + str(b[v])
   sys.stdout.write(chr(int(c,2)))
   n = 0
   c = ""
- elif n == min:
+ elif m == min:
   b[n] = 0
   n = n+1
- elif n == (max-1):
+ elif m == (max-1):
   b[n] = 1
   n = n+1
