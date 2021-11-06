6307c316aa34743c95039b148cc555ff022c3f9a
Newa <chezsick@orange.fr>
2021-11-06 16:31:54 +0100

    fix it

--- radio/radiohash.py
+++ radio/radiohash.py
@@ -2,15 +2,17 @@ import sys
 from struct import unpack
 min = int(sys.argv[1])
 max = int(sys.argv[2])
-len = int(sys.argv[3])
-hash = [0] * len
+lng = int(sys.argv[3])
+hash = [0] * lng
 while True:
  o = sys.stdin.read(4)
+ if not len(o) == 4:
+  break
  n = unpack("I",o)[0]
- if n >= min and n < max:
-  i = int((len*(n-min)/(float(max)-float(min))))
+ if (n >= min) and (n < max):
+  i = int((lng*(n-min)/(float(max)-float(min))))
   hash[i] = (hash[i] + 1) % 10
 
-for u in range(len):
+for u in range(lng):
  sys.stdout.write("%s" %(hash[u]))
 sys.stdout.write("\n")
