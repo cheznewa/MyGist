3e09798eb80dc814d78d057910aee854bb820001
Newa <chezsick@orange.fr>
2021-11-16 00:44:44 +0100

    update fontttf now support for otf dump

--- pricecalc/fontttf.py
+++ pricecalc/fontttf.py
@@ -1,12 +1,25 @@
 import sys
 import re
 price=0
+st=False
 for o in sys.stdin:
  if re.search("<TTGlyph ",o):
   price=price/1.1
  if re.search("<pt ",o):
   if re.search(" on=\"0\"",o):
    price=price+0.2
-  else:
+  if re.search(" on=\"1\"",o):
    price=price+0.1
+ if re.search("<CharString ",o):
+  st = True
+  price=price/1.1
+ if re.search("lineto",o) and st:
+  price=price+0.1
+ if re.search("moveto",o) and st:
+  price=price+0.3
+ if re.search("curveto",o) and st:
+  price=price+0.5
+ if re.search("endchar",o) and st:
+  st = False
+
 sys.stdout.write("%s\n" %(price))
