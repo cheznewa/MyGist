5815c1b776730e5a6f2a44b9adde3c9c421a8c9d
Newa <chezsick@orange.fr>
2021-11-21 10:41:30 +0100

    add the possiblity to add real logo inside!

--- logicemu2embed.bash
+++ logicemu2embed.bash
@@ -3,13 +3,21 @@
 # extract :: tail -c sizereadquotienttaiascar thepngfileasembed.png | lzcomp -d
 img=$(mktemp)
 circ=$(mktemp)
+logo=$(mktemp)
 t=$(mktemp)
 w=$(mktemp)
 x=$(mktemp)
 name=$1
+logo=$2
 if test -z $name
 then
 name=CircuitSansNom
+fi
+if test -n $logo
+then
+curl -o $logo https://raw.githubusercontent.com/gilbarbara/logos/master/logos/${logo}.svg
+logoblacked=$(mktemp)
+python2 $MYGIST/coloreffectreplacer/coloreffectreplacer.py sub 255 255 255 < $logo > $logoblacked
 fi
 convert -size 200x50 xc:none -stroke black -fill white -draw "rectangle 0,0 199,49 circle 25,25 15,25" miff:$img
 convert -font OCRA -pointsize 9 -background none "label:$name" miff:- | composite -gravity center - $img miff:$t
@@ -19,5 +27,10 @@ r=$(lzcomp -d < $circ | wc -c)
 n=$(lzcomp -d < $circ | python2 $MYGIST/logicemu2luxar.py)
 convert -font OCRA -pointsize 9 -background none "label:${s}/${r} Car" miff:- | composite -gravity southeast - $t miff:$w
 convert -font OCRA -pointsize 9 -background none "label:${n}" miff:- | composite -gravity northeast - $w png:$x
+if test -n $logo
+then
+rsvg-convert -z 0.1 $logoblacked | composite -gravity east - $x png:$x
+rm $logoblacked
+fi
 cat $x $circ
-rm $img $circ $t $x $w
+rm $img $circ $t $x $w $logo
