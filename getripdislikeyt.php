0d340abd82ceb61918443ae0eb6afa358f0d563f
Newa <chezsick@orange.fr>
2021-11-27 23:23:20 +0100

    update the get rip dislike yt

--- getripdislikeyt.php
+++ getripdislikeyt.php
@@ -1,5 +1,6 @@
 <?php
 // https://archive.org/details/Youtube_dislike_count_export_2021-11-18
+// zstd -dcq /path/to/dislikes_export_2021_11_18.txt.xz | php getripdislikeyt.php TonIDA11Car
 if (11 !== strlen($argv[1]))
 {
 echo "Une ID De Vidéo A 11 Caractère, C'est Juste Que Ca, Ce Que Je Demande\n";
@@ -18,3 +19,4 @@ exit(intval($expl[5]));
 }
 }
 echo "Désolé, Ton ID N'existe Pas :(\n";
+exit(3);
