430aa2b235dc724a2c36938cbeaf4db041239b52
Newa <chezsick@orange.fr>
2021-12-05 10:20:55 +0100

    update beastnumbercheck

--- beastnumbercheck.py
+++ beastnumbercheck.py
@@ -1,32 +1,36 @@
 import sys
+if len(sys.argv) == 2:
+ u = int(sys.argv[1])
+else:
+ u = 666
 o = sys.stdin.read()
 s = 0
 f = 0
 for n in range(999):
  for i in o:
   s = (s+ord(i)+n) %999
- if s == 666:
+ if s == u:
   sys.stdout.write("Beast Number Found At %s Sum\n" %(n))
   f = f+1
  s = 0
 for n in range(999):
  for i in o:
   s = (1+(s+ord(i)*n)) %999
- if s == 666:
+ if s == u:
   sys.stdout.write("Beast Number Found At %s Times\n" %(n))
   f = f+1
  s = 0
 for n in range(999):
  for i in o:
   s = (s+pow(ord(i),n,999))%999
- if s == 666:
+ if s == u:
   sys.stdout.write("Beast Number Found At %s Power\n" %(n))
   f = f+1
  s = 0
 for n in range(999):
  for i in o:
   s = (s+pow(n,ord(i),999))%999
- if s == 666:
+ if s == u:
   sys.stdout.write("Beast Number Found At %s Expo\n" %(n))
   f = f+1
  s = 0
