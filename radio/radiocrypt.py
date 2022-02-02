64664f525d50f6b1deca60120e3d376826043cf6
cheznewa <chezsick@orange.fr>
2022-02-02 02:42:36 +0100

    add after argv in radiocrypt

--- radio/radiocrypt.py
+++ radio/radiocrypt.py
@@ -1,6 +1,11 @@
 import sys
 mode = sys.argv[1]
+after = int(sys.argv[5])
 i = 0
+while after:
+ i = (int(sys.argv[2])*i+int(sys.argv[3])) % int(sys.argv[4])
+ after=after-1
+
 while True:
  o = sys.stdin.read(2)
  i = (int(sys.argv[2])*i+int(sys.argv[3])) % int(sys.argv[4])
