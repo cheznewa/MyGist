edc26e865cfa83ac3cc384c5b1dc181e59f7b488
cheznewa <chezsick@orange.fr>
2022-02-02 22:38:41 +0100

    update file2biblio

--- file2biblio.py
+++ file2biblio.py
@@ -1,17 +1,21 @@
 import sys
-from base58 import b58encode_int
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ-+"
 import hashlib
 hash = hashlib.sha256()
 hash.update(sys.stdin.read())
 r = hash.digest()
 w = 1+ord(r[0])%6
 s = 1+ord(r[1])%8
-v = 1+ord(r[2])%32
+v = 1+ord(r[2])%64
 n = int(sys.argv[1])
 a = 0
 for m in range(n):
  a = a + ord(r[m+3])*pow(256,m)
-print("Address :: " + b58encode_int(a))
-print("Wall :: " + str(w))
-print("Shelf :: " + str(s))
-print("Volume :: " + str(v))
+t = ""
+while a:
+ a,q = divmod(a,28)
+ t = alpha[q] + t
+sys.stdout.write("Address :: %s\n" %(t))
+sys.stdout.write("Wall :: %s\n" %(w))
+sys.stdout.write("Shelf :: %s\n" %(s))
+sys.stdout.write("Volume :: %s\n" %(v))
