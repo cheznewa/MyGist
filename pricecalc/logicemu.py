012e1f51f9aa319e634b9efec816d0b7bd7fc960
cheznewa <chezsick@orange.fr>
2022-02-06 00:55:57 +0100

    update

--- pricecalc/logicemu.py
+++ pricecalc/logicemu.py
@@ -1,20 +1,24 @@
 import sys
 import re
 price = 0
+v = True
 while True:
  o = sys.stdin.read(1)
- if (o == "<") or (o == ">") or (o == "v") or (o == "^"):
-  price=price+3
- elif (o == "[") or (o == "]") or (o == "w") or (o == "m"):
-  price=price+5
- elif o == "i":
-  price=price+10
- elif o == "I":
-  price=price+100
- elif o == " ":
-  price=price/1.1
- else:
-  price=price+1
+ if "\"" == o:
+  v = not(v)
+ if v:
+  if (o == "<") or (o == ">") or (o == "v") or (o == "^"):
+   price=price+3
+  elif (o == "[") or (o == "]") or (o == "w") or (o == "m"):
+   price=price+5
+  elif o == "i":
+   price=price+10
+  elif o == "I":
+   price=price+100
+  elif o == " ":
+   price=price/1.1
+  else:
+   price=price+1
  if not o:
   break
 
