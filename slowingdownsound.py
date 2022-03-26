16216a4e7684713199234595f240724f4d5fa374
cheznewa <chezsick@orange.fr>
2022-03-26 01:00:25 +0100

    add slowing down sound effect

---
+++ slowingdownsound.py
@@ -0,0 +1,16 @@
+import sys
+rate = int(sys.argv[1])
+leng = int(sys.argv[2])
+slow = int(sys.argv[3])
+byte = int(sys.argv[4])
+n = 0
+l = 1.0
+while True:
+ if l>(l*n)%1:
+  a = sys.stdin.read(byte)
+ if n >= leng*rate:
+  l = l-(1/(float(slow)*float(rate)))
+ sys.stdout.write(a)
+ n = n+1
+ if n > (leng+slow)*rate:
+  break
