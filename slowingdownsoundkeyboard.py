511a8ffa4d8fcce4d7004bcb9170428e9b16522f
cheznewa <chezsick@orange.fr>
2022-04-01 08:39:42 +0200

    add slowing down effect keyboard controlled

--- slowingdownsoundkeyboard.py
+++ slowingdownsoundkeyboard.py
@@ -1,16 +1,23 @@
 import sys
+import keyboard
 rate = int(sys.argv[1])
 byte = int(sys.argv[2])
 slow = float(sys.argv[3])
-leng = float(sys.argv[4])
 n = 0
 l = 1.0
+u = 0
+a = False
 while True:
  if l>(l*n)%1:
   o = sys.stdin.read(byte)
- if n >= leng*rate:
+ if a:
   l = l-(1/(float(slow)*float(rate)))
+  if n > m+(rate*slow):
+   break
  sys.stdout.write(o)
+ if u == 0:
+  if keyboard.is_pressed("down"):
+   a = True
+   m = n
  n = n+1
- if n > (leng+slow)*rate:
-  break
+ u = (u+1)%10000
