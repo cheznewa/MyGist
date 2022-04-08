cde9759c32fb60a825240b458577a9a4e7315ac0
cheznewa <chezsick@orange.fr>
2022-04-08 05:59:50 +0200

    add random circuit for logicemu

---
+++ randomlogicemucircuit.py
@@ -0,0 +1,22 @@
+import sys
+from random import sample,randint
+nand = int(sys.argv[1])
+regs = int(sys.argv[2])
+inpu = int(sys.argv[3])
+outp = int(sys.argv[4])
+gate = sys.argv[5]
+
+for n in range(inpu):
+ sys.stdout.write("%sg-s\n@\n" %(n))
+for n in range(inpu,+inpu+outp):
+ sys.stdout.write("l<g%s\n@\n" %(n))
+
+h = nand
+
+while nand:
+ sys.stdout.write("#-g%s\n#<g%s\n%s<g%s\n@\n" %(nand+inpu,randint(1,nand+inpu+outp),gate,randint(1,nand+inpu+outp)))
+ nand=nand-1
+
+while regs:
+ sys.stdout.write("#-g%s\nd<g%s\nc<g%s\n@\n" %(h+regs+inpu+outp,randint(1,regs+inpu+outp),randint(1,regs+inpu+outp)))
+ regs=regs-1
