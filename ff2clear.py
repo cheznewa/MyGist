fbc743804067e3faa840ba43f63283b82640f500
Newa <chezsick@orange.fr>
2022-07-03 12:39:57 +0200

    fix it and update ff2clear

--- ff2clear.py
+++ ff2clear.py
@@ -11,12 +11,25 @@ sys.stdout.write(size)
 rr=0
 gg=0
 bb=0
+if mode == "cc":
+ ahe = sys.argv[2]
+ bhe = sys.argv[3]
+ ra = int(ahe[0:2],16)
+ ga = int(ahe[2:4],16)
+ ba = int(ahe[4:6],16)
+ rb = int(bhe[0:2],16)
+ gb = int(bhe[2:4],16)
+ bc = int(bhe[4:6],16)
 while bool(n):
  o = sys.stdin.read(8)
  r = (ord(o[0])*256)+ord(o[1])
  g = (ord(o[2])*256)+ord(o[3])
  b = (ord(o[4])*256)+ord(o[5])
  a = (ord(o[6])*256)+ord(o[7])
+ if not bool(n%x):
+  rr=r
+  gg=g
+  bb=b
  dr=r-rr
  dg=g-gg
  db=b-bb
@@ -60,6 +73,15 @@ while bool(n):
   elif d > 0:
    rv = max(r - abs(d),0)
    gv = max(g - abs(d),0)
+ if mode == "cc":
+  if d < 0:
+   rv = min(r + (int((ra/255.0)*abs(d))),65535)
+   gv = min(g + (int((ga/255.0)*abs(d))),65535)
+   bv = min(b + (int((ba/255.0)*abs(d))),65535)
+  elif d > 0:
+   rv = max(r - (int((1-(rb/255.0))*(abs(d)))),0)
+   gv = max(g - (int((1-(gb/255.0))*(abs(d)))),0)
+   bv = max(b - (int((1-(bc/255.0))*(abs(d)))),0)
  sys.stdout.write("%s%s%s%s%s%s%s%s" %(chr(rv/256),chr(rv%256),chr(gv/256),chr(gv%256),chr(bv/256),chr(bv%256),o[6],o[7]))
  rr=r
  gg=g
