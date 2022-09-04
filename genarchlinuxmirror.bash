6b8b5585fb0543d92ff0930e8ca979e27bc71430
Newa <chezsick@orange.fr>
2022-09-04 11:07:40 +0200

    fix it

--- genarchlinuxmirror.bash
+++ genarchlinuxmirror.bash
@@ -1,10 +1,16 @@
 function genlist()
 {
 printf "<h1>Index : $1</h1>\n"
+printf "<a href=\"../index.html\">..</a><br/>\n"
 for a in $1/*
 do
 b=$(basename "$a")
+if test -d "$a"
+then
+printf "<a href=\"$b/index.html\">$b</a><br/>\n"
+else
 printf "<a href=\"$b\">$b</a><br/>\n"
+fi
 done
 }
 rsync --exclude=pool/* --exclude=iso/* --exclude=images/* -v --copy-links -r rsync://ftp.rnl.tecnico.ulisboa.pt/pub/archlinux /tmp/archlinux
