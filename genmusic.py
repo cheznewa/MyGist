c04d8ac508dfa0859955b4bd9b04127a2d612a52
Newa <chezsick@orange.fr>
2022-09-06 09:51:58 +0200

    make GenMusic Infinite

--- genmusic.py
+++ genmusic.py
@@ -2,8 +2,7 @@ import random
 import sys
 random.seed(sys.argv[1])
 note = sys.stdin.read()
-i = int(sys.argv[2])
-while i:
+while True:
  maj = random.randint(1,16)
  n = random.randint(16,64)
  while n:
@@ -14,4 +13,3 @@ while i:
    sys.stdout.write(note[int(j)])
    j = k+j
   n=n-1
- i=i-1
