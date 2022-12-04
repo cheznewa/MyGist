a8537ad9519293579efcfc391463a0666a95110b
Newa <chezsick@orange.fr>
2022-12-04 12:04:37 +0100

    update fontapexffjs

--- fontapexffjs.bash
+++ fontapexffjs.bash
@@ -1,15 +1,47 @@
 # Command Example :::::::::::::::::::::::::::: FONT_APEX=/path/to/font-apex/svgs bash fontapexffjs.bash 000000 ffffff 1
+# Command Example With Modifier :::::::::::::::::::::::::::: FONT_APEX=/path/to/font-apex/svgs bash fontapexffjs.bash 000000 ffffff 1
 # Source ::::::::::::::::::::::::::::::::::::: https://github.com/oracle/font-apex/
+if [[ -z $FONT_APEX ]]
+then
+exit 1
+fi
 color=$1
-bgcol=$2
-pntsz=$3
+pntsz=$2
+modif=$3
+clmod=$4
+if [[ -z $color ]]
+then
+color="000000"
+pntsz=1
+fi
+if [[ -n $modif ]]
+then
+if [[ -z $clmod ]]
+then
+clmod="000000"
+fi
+tmp=$(mktemp)
+sed "s/<path /<path fill=\"#$clmod\" /g" < $FONT_APEX/small/modifier-$modif.svg | rsvg-convert -z $pntsz | convert png:- -resize 50% miff:$tmp
 for f in $FONT_APEX/small/*.svg
 do
 printf "var apex_s_$(basename ${f} .svg | tr - _) = "
-sed "s/<path /<path fill=\"#$color\" /g" < $f | rsvg-convert -b \#$bgcol -z $pntsz | png2ff | base256 --format=js
+sed "s/<path /<path fill=\"#$color\" /g" < $f | rsvg-convert -z $pntsz | composite -gravity southeast miff:$tmp png:- png:- | png2ff | base256 --format=js
+done
+sed "s/<path /<path fill=\"#$clmod\" /g" < $FONT_APEX/large/modifier-$modif.svg | rsvg-convert -z $pntsz | convert png:- -resize 50% miff:$tmp
+for f in $FONT_APEX/large/*.svg
+do
+printf "var apex_l_$(basename ${f} .svg | tr - _) = "
+sed "s/<path /<path fill=\"#$color\" /g" < $f | rsvg-convert -z $pntsz | composite -gravity southeast miff:$tmp png:- png:- | png2ff | base256 --format=js
+done
+else
+for f in $FONT_APEX/small/*.svg
+do
+printf "var apex_s_$(basename ${f} .svg | tr - _) = "
+sed "s/<path /<path fill=\"#$color\" /g" < $f | rsvg-convert -z $pntsz | png2ff | base256 --format=js
 done
 for f in $FONT_APEX/large/*.svg
 do
 printf "var apex_l_$(basename ${f} .svg | tr - _) = "
-sed "s/<path /<path fill=\"#$color\" /g" < $f | rsvg-convert -b \#$bgcol -z $pntsz | png2ff | base256 --format=js
+sed "s/<path /<path fill=\"#$color\" /g" < $f | rsvg-convert -z $pntsz | png2ff | base256 --format=js
 done
+fi
