abbb40fca22627d69252c8aee8aa5dd5e94dffed
Newa <chezsick@orange.fr>
2022-12-24 23:22:16 +0100

    add encryption using openssl in chat and gsmphone piping

--- chatpiping.bash
+++ chatpiping.bash
@@ -1,16 +1,18 @@
-# Required :::::::::::::::::::: https://quut.com/gsm/
-# And SoX
-up=$1
-down=$2
+up=$2
+down=$1
 server=$3
 if [[ -z $server ]]
 then
 server="https://ppng.io"
 fi
+enc=$(tr -cd 0-9 < /dev/urandom | head -c 100)
+curl -s -T - ${server}/${up}_chat <<< $enc > /dev/null &
+sleep 1
+dec=$(curl -s ${server}/${down}_chat)
 while test 1
 do
 read -p "you : " send
-curl -s -T - ${server}/${up}_chat <<< "$send" > /dev/null &
+openssl rc4 -pbkdf2 -k $enc <<< "$send" | curl -s -T - ${server}/${up}_chat > /dev/null &
 printf "$down : "
-curl -s ${server}/${down}_chat
+curl -s ${server}/${down}_chat | openssl rc4 -pbkdf2 -d -k $dec
 done
--- gsmphonepiping.bash
+++ gsmphonepiping.bash
@@ -1,15 +1,19 @@
 # Required :::::::::::::::::::: https://quut.com/gsm/
 # And SoX
-up=$1
-down=$2
+up=$2
+down=$1
 server=$3
 if [[ -z $server ]]
 then
 server="https://ppng.io"
 fi
+enc=$(tr -cd 0-9 < /dev/urandom | head -c 100)
+curl -s -T - ${server}/${up}_gsmphone <<< $enc > /dev/null &
+sleep 1
+dec=$(curl -s ${server}/${down}_gsmphone)
 while test 1
 do
-rec -q -t s16 -r 8000 -c 1 - trim 0 1 | toast -l | curl -s -T - ${server}/${up}_gsmphone > /dev/null &
-curl -s ${server}/${down}_gsmphone | toast -d -l | play -q -t s16 -r 8000 -c 1 - trim 0 1 &
+rec -q -t s16 -r 8000 -c 1 - trim 0 1 | toast -l | openssl rc4 -pbkdf2 -k $enc | curl -s -T - ${server}/${up}_gsmphone > /dev/null &
+curl -s ${server}/${down}_gsmphone | openssl rc4 -pbkdf2 -d -k $dec | toast -d -l | play -q -t s16 -r 8000 -c 1 - trim 0 1 &
 sleep 1
 done
