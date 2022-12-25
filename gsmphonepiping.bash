02df6076ec4c220e1d51abc3fb4fe0444890d3a1
Newa <chezsick@orange.fr>
2022-12-25 23:23:34 +0100

    add custom delay on gsmphonepiping

--- gsmphonepiping.bash
+++ gsmphonepiping.bash
@@ -2,7 +2,8 @@
 # And SoX
 up=$1
 down=$2
-server=$3
+delay=$3
+server=$4
 if [[ -z $MYGIST ]]
 then
 exit 1
@@ -11,13 +12,13 @@ if [[ -z $server ]]
 then
 server="https://ppng.io"
 fi
-enc=$(seq 0 255 | shuf | tr "\n" " ")
+enc=$(shuf -i 0-255 | tr "\n" " ")
 curl -s -T - ${server}/${up}_gsmphone <<< $enc > /dev/null &
 sleep 1
 dec=$(curl -s ${server}/${down}_gsmphone)
 while test 1
 do
-rec -q -t s16 -r 8000 -c 1 - trim 0 1 | toast -l | python2 $MYGIST/rc4.py $enc | curl -s -T - ${server}/${up}_gsmphone > /dev/null &
+rec -q -t s16 -r 8000 -c 1 - trim 0 $delay | toast -l | python2 $MYGIST/rc4.py $enc | curl -s -T - ${server}/${up}_gsmphone > /dev/null &
 curl -s ${server}/${down}_gsmphone | python2 $MYGIST/rc4.py $dec | toast -d -l
-sleep 1
+sleep $delay
 done | play -q -t s16 -r 8000 -c 1 -
