cf810b77f8eb58a6fc1919b66dca23b8c5487b9c
Newa <chezsick@orange.fr>
2023-02-04 16:55:59 +0100

    fix it

--- ff2likesilicon.py
+++ ff2likesilicon.py
@@ -6,15 +6,17 @@ size = sys.stdin.read(8)
 x = (pow(256,3)*ord(size[0]))+(pow(256,2)*ord(size[1]))+(pow(256,1)*ord(size[2]))+(pow(256,0)*ord(size[3]))
 y = (pow(256,3)*ord(size[4]))+(pow(256,2)*ord(size[5]))+(pow(256,1)*ord(size[6]))+(pow(256,0)*ord(size[7]))
 n = x*y
+m = n
 sys.stdout.write("farbfeld")
 sys.stdout.write(size)
 while bool(n):
+ e = m-n
  o = sys.stdin.read(8)
  r = (ord(o[0])*256)+ord(o[1])
  g = (ord(o[2])*256)+ord(o[3])
  b = (ord(o[4])*256)+ord(o[5])
- a = n%x
- z = n/x
+ a = e%x
+ z = e/x
  sr = int(65535*(a/float(x)))
  sg = int(65535*(z/float(y)))
  sb = 65535-int(65535*(a/float(x)))
