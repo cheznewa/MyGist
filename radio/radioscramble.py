e8a4d8403df67bea36a9869d7449cdd21a1f9022
Newa <chezsick@orange.fr>
2023-02-07 18:55:39 +0100

    update radio scramble (x2)

--- radio/radioscramble.py
+++ radio/radioscramble.py
@@ -6,6 +6,6 @@ i=0
 while True:
  o = sys.stdin.read(2)
  r = ord(o[1])*256 + ord(o[0])
- r = r ^ (int(65535*dose)*i)
+ r = int(((r ^ (65535*i))*dose)+(r*(1-dose)))
  i = (i+1)%2
  sys.stdout.write("%s%s" %(chr(r % 256),chr(r / 256)))
--- radio/radioscrambletransition.py
+++ radio/radioscrambletransition.py
@@ -4,7 +4,6 @@ rate = int(sys.argv[1])
 pase = float(sys.argv[2])
 leng = float(sys.argv[3])
 
-i=0
 l=0
 n=0
 m=0
@@ -15,7 +14,7 @@ while True:
  l=min(float(m)/(leng*rate),1)
  o = sys.stdin.read(2)
  r = ord(o[1])*256 + ord(o[0])
- r = r ^ (int(65535*l)*i)
- i = (i+1)%2
+ i = n%2
+ r = int(((r ^ (65535*i))*l)+(r*(1-l)))
  sys.stdout.write("%s%s" %(chr(r % 256),chr(r / 256)))
  n=n+1
