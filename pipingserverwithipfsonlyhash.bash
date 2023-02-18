30d28b6893405fd1521509a7883a735e900578cd
Newa <chezsick@orange.fr>
2023-02-18 20:22:32 +0100

    add pipingserverwithipfsonlyhash

---
+++ pipingserverwithipfsonlyhash.bash
@@ -0,0 +1,18 @@
+# Require ::::::: https://github.com/alanshaw/ipfs-only-hash
+count=$1
+server=$2
+if [[ -z $server ]]
+then
+server="https://ppng.io"
+fi
+tmp=$(mktemp)
+cat - > $tmp
+n=0
+addr=$(ipfs-only-hash --cid-version 0 $tmp)
+printf "${server}/$addr\n"
+while [[ $count -gt 0 ]]
+do
+curl -s -T $tmp ${server}/$addr > /dev/null
+count=$(($count - 1))
+done
+rm $tmp
