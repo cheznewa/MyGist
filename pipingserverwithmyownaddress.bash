5bbe2cae255a5055f18d4e47e20e72c0808bef1f
Newa <chezsick@orange.fr>
2023-02-18 20:56:04 +0100

    add pipingserverwithmyownaddress

---
+++ pipingserverwithmyownaddress.bash
@@ -0,0 +1,25 @@
+# Require ::::::: https://github.com/keis/base58 ;;; https://github.com/maandree/sha3sum
+count=$1
+server=$2
+if [[ -z $server ]]
+then
+server="https://ppng.io"
+fi
+tmp=$(mktemp)
+hmp=$(mktemp)
+cat - > $tmp
+n=0
+split -a 16 -d -b 1024 $tmp ${tmp}_
+for h in ${tmp}_*
+do
+cat $h $hmp | sha3-256sum -b > $hmp
+done
+rm ${tmp}_*
+addr=$(base58 $hmp)
+printf "${server}/ppng${addr}\n"
+while [[ $count -gt 0 ]]
+do
+curl -s -T $tmp ${server}/ppng${addr} > /dev/null
+count=$(($count - 1))
+done
+rm $tmp $hmp
