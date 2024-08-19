07f145b825d599f45dd2f7ec46d2ec911d4a2ad6
Newa <chezsick@orange.fr>
2024-08-19 18:51:39 +0200

    update newa's operator

--- functionlogicemu/newaoperator.js
+++ functionlogicemu/newaoperator.js
@@ -1,11 +1,10 @@
-var a = 8;
 registerAluOverride(0,
 function(inputs,outputs)
 {
 var r = 0;
-for (var h=0;h<a;h++)
+for (var h=0;h<inputs.length;h++)
 {
-outputs[h] = (inputs[h] | inputs[h+a]) ^ r;
-r = inputs[h] & inputs[h+a];
+outputs[h] = (inputs[h*2] | inputs[(h*2)+1]) ^ r;
+r = inputs[h*2] & inputs[(h*2)+1];
 }
 });
